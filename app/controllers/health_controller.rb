8b6ba5d870cb90bc0cfb4457b948f20829bbe26a
taichi-k <no3b1219@gmail.com>
2025-12-12 00:39:14 +0900

    spanを追加しtraceをわかりやすく

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,9 +1,17 @@
 class HealthController < ApplicationController
   def index
-    sleep 0.1
     tracer = OpenTelemetry.tracer_provider.tracer('manual-test')
     tracer.in_span('health-check-manual-span') do |span|
+      sleep 0.1
       span.add_event('health endpoint called')
+    end
+    tracer.in_span("call_external_api") do |_span|
+      # ダミーとして sleep や HTTP クライアントを呼ぶ
+      sleep 0.2
+    end
+    tracer.in_span("call_external_api_another") do |_span|
+      # ダミーとして sleep や HTTP クライアントを呼ぶ
+      sleep 0.5
     end
     render json: { status: "ok", time: Time.current }
   end
